9f00b12906e0409b03088ef0305e9db425676d6b
Aditya <aditya.jha2020123@gmail.com>
2023-11-22 12:47:08 -0500

    Final Version. All features implemented

--- README.md
+++ README.md
@@ -1,40 +1,50 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *Crowdfunder*
 
-Submitted by: **Your Name Here**
+Submitted by: **Aditya Jha**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**Crowdfunder** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **3** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [x] The introduction section explains the background of the company and how many games remain unfunded.
+* [x] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [x] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [x] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
 
-* [ ] List anything else that you can get done to improve the app functionality!
+* [x] List anything else that you can get done to improve the app functionality!
+* [x] Implemented basic button shadow to make the filter buttons feel more button-like instead of being static.
+* [x] Implemented a basic nav bar using datalists. In this navabar, the user can type the name of a section or select one from the options given.
+* [x] Implemented a Go button to wrap the user to the desired section of the text.
+* [x] Implemented a donate now button for every game card. 
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://imgur.com/a/dOqvOUi' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 <!-- Replace this with whatever GIF tool you used! -->
 GIF created with ...  
 <!-- Recommended tools:
 [Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
+
 [peek](https://github.com/phw/peek) for Linux. -->
+[ScreenToGif](https://www.screentogif.com/) for Windows
 
 ## Notes
 
 Describe any challenges encountered while building the app.
+
+While building this webpage, I had to spend sometime trying to create a navbar that did not feel very out of place.
+I tried using some css on it. But it felt weird so I refrained from using css in the final version.
+
+I also had some difficulties with the reduce function as I had never used it before so I had to spend sometime learning about it before I could complete the challenge.
 
 ## License
 
--- index.js
+++ index.js
@@ -29,25 +29,40 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for(var i=0;i<games.length;i++)
+    {
+        
+    
 
         // create a new div element, which will become the game card
-
+        let game_class = document.createElement('div');
+        
+        
 
         // add the class game-card to the list
-
+        game_class.classList.add('game-card');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        game_class.innerHTML = `<img src=${games[i]["img"]} class = "game-img"> 
+        </br><h3> ${games[i]["name"]}</h3>
+        <p>${games[i]["description"]}</p>
+        <p>Goal:${games[i]["goal"]}</br></br>
+        Plegded:${games[i]["pledged"]}</br>
+        <button>Donate Now</button>`;
+        // console.log(`hello:${i} ${games[i]["name"]}`);
 
         // append the game to the games-container
-
+        let container = document.getElementById("games-container");
+        container.appendChild(game_class);
+    }
 }
 
 // call the function we just defined using the correct variable
+ addGamesToPage(GAMES_JSON) ;
+
 // later, we'll call this function using a different list of games
 
 
@@ -62,19 +77,28 @@ const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
 
+//here I broke down the JSON to demonstrate ability to use the map function
+let sumContribute = GAMES_JSON.map(GAMES_JSON  => GAMES_JSON.backers);
+let TotalsumContribute = sumContribute.reduce((prev,next)=>{ return prev+next},0);
+
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${TotalsumContribute.toLocaleString('en-US')}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+//here I did it like it was shown in the example on the prework instruction site
+let TotalAmountRaised = GAMES_JSON.reduce((prev,gameObj)=>{ return prev+gameObj.pledged},0);
 
+// set inner HTML using template literal
+raisedCard.innerHTML = ` $ ${TotalAmountRaised .toLocaleString('en-US')}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+let TotalgameCrds = GAMES_JSON.reduce((prev)=>{ return prev+1},0);
+// set inner HTML using template literal
+gamesCard.innerHTML = `${TotalgameCrds .toLocaleString('en-US')}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -87,10 +111,9 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    let unFunded = GAMES_JSON.filter((games)=>{return games.pledged<games.goal});
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unFunded);
 }
 
 // show only games that are fully funded
@@ -98,18 +121,18 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let fullFunded = GAMES_JSON.filter((games)=>{return games.pledged>=games.goal});
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fullFunded);
 }
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
-
+    
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON) ;
 }
 
 // select each button in the "Our Games" section
@@ -117,9 +140,11 @@ const unfundedBtn = document.getElementById("unfunded-btn");
 const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
+
 // add event listeners with the correct functions to each button
-
-
+unfundedBtn.addEventListener("click",filterUnfundedOnly);
+fundedBtn.addEventListener("click",filterFundedOnly);
+allBtn.addEventListener("click",showAllGames);
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
  * Skills used: template literals, ternary operator
@@ -129,13 +154,20 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+//reused line of code from above but with some slight tweaks to just get the number
+let unFundedGames = (GAMES_JSON.filter((games)=>{return games.pledged<games.goal})).length;
+let allGames = GAMES_JSON.length;
+console.log(unFundedGames);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = `A total of $${TotalAmountRaised.toLocaleString('en-US')} has been raised for ${allGames} 
+${allGames == 1 ? "game. ": "games. "}Currently, ${unFundedGames} 
+${unFundedGames == 1 ? "game remains unfunded. ": "games remain unfunded. "} We need your help to fund these amazing games!`
 
 // create a new DOM element containing the template string and append it to the description container
-
+let descript = document.createElement('p');
+descript.innerHTML = displayStr;
+descriptionContainer.appendChild(descript);
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
  * Skills used: spread operator, destructuring, template literals, sort 
@@ -147,9 +179,40 @@ const secondGameContainer = document.getElementById("second-game");
 const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
     return item2.pledged - item1.pledged;
 });
-
+console.log(sortedGames);
 // use destructuring and the spread operator to grab the first and second games
-
+let [firstGame,SecondGame] = [sortedGames[0],sortedGames[1]]; 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+let game1 = document.createElement('p');
+game1.innerHTML = `<b>${firstGame.name}</b>`
+firstGameContainer.appendChild(game1);
 
 // do the same for the runner up item
+let game2 = document.createElement('p');
+game2.innerHTML = `<b>${SecondGame.name}</b>`
+secondGameContainer.appendChild(game2);
+
+//this feature allows for a quick scroll down to a desired part of the web page. Right now it's a small webpage so it does not matter much
+//but one day it will
+function inputVal(){
+    //stores whatever input the user put in the text box
+    let visitInput = document.getElementById("input").value;
+    console.log(visitInput);
+
+    //basic if and else statment to see where the page scrolls down to
+    if(visitInput == "Our Game")
+    {
+        const scroll = document.getElementById("games-container");
+        scroll.scrollIntoView();
+    }
+    else if(visitInput == "Stats")
+    {
+        const scroll = document.getElementById("Stats");
+        scroll.scrollIntoView();
+    }
+}
+//find the button.
+const inputbtn = document.getElementById("go");
+//when button is clicked call the inputVal 
+inputbtn.addEventListener("click",inputVal);
+
